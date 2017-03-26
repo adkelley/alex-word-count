cc94e2479ac1f5b17dbf2e033c5d718095268598
Alex Kelley <adkelley@mac.com>
2017-03-26 11:58:28 -0700

    first commit

---
+++ spec/alex-word-count-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AlexWordCountView from '../lib/alex-word-count-view';
+
+describe('AlexWordCountView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
